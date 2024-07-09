9a49cecd303df7a688f6d27738849c46b02fbb94
Razzakh <mrazzakh123@gmail.com>
2024-07-09 08:32:44 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
+    let weather_data={ temperature: 25, conditions: "Sunny" };
+
   return (
     <div>
+      <WeatherDisplay weather_data={weather_data} />
         {/* Do not remove the main div */}
     </div>
   )
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+const WeatherDisplay = (props) => {
+    let a=null;
+    if (props.weather_data.temperature>20) {
+        a={color: 'red'};
+    }else{
+        a={color: 'blue'};
+    }
+  return (
+    <div>
+        <p>Temperature:<span style={a}>{props.weather_data.temperature} </span> </p>
+<p>Conditions:{props.weather_data.conditions} </p>
+    </div>
+  )
+}
+
+export default WeatherDisplay
